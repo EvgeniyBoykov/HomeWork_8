7c13075e7bf9370295c7f047be9f7836c36bb6a9
EvgeniyBoykov <88290040+EvgeniyBoykov@users.noreply.github.com>
2021-08-29 23:52:26 +0400

    Initial commit

---
+++ src/HomeWork_8.java
@@ -0,0 +1,37 @@
+import java.nio.charset.StandardCharsets;
+
+public class HomeWork_8  {
+    public static void main(String[] args) {
+        String s1 = "s1";
+        String s2 = new String("s2");
+        String s3 = new String(s2);
+
+        char[] symbols = { 'A', 'B', 'C'};
+        String s4 = new String(symbols);
+
+        byte[] bytes = {65,66,64};
+        String s5 = new String (bytes);
+        String s6 = new String(bytes, StandardCharsets.UTF_16);
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+        System.out.println(s5);
+        System.out.println(s6);
+
+        String s7 = "Десять: " + (5 + 5);
+        System.out.println(s7);
+
+        StringBuilder sb = new StringBuilder();
+        long startTime = System.nanoTime();
+        for (int i = 0; i < 300000; i++) {
+        sb.append("a");
+        }
+        long endTime = System.nanoTime();
+        float result = (endTime - startTime) * 0.000000001f;
+        System.out.println(result);
+
+        new MyWindow();
+    }
+}
---
+++ src/MyWindow.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyWindow extends JFrame {
+    public MyWindow() {
+        setLocation(350, 400);
+        setSize(600, 400);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel(new GridLayout(1,2));
+
+        JButton btn1 = new JButton("Push the button");
+        JButton btn2 = new JButton("Exit");
+        panel.add(btn1);
+        panel.add(btn2);
+
+        ActionListener actionListener = new CloseActionListener();
+        btn2.addActionListener(actionListener);
+
+        add(panel, BorderLayout.SOUTH);
+
+        setVisible(true);
+    }
+}
